16a4f3bc264f16ec412ad2332f82668451c77b9a
Plan C <hubenchang0515@outlook.com>
2024-09-28 16:43:56 +0800

    EnableHighDpiScaling

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ using namespace TowFATool;
 
 int main(int argc, char* argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app{argc, argv};
     MainWindow window;
     window.show();
